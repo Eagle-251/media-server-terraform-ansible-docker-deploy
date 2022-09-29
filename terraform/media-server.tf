20163bd3e0ee58becf4340c621391c94667690c9
Ewan <ewan@gilchrist.scot>
2022-09-29 22:46:08 +0200

    Cleaned up media-server.tf

--- terraform/media-server.tf
+++ terraform/media-server.tf
@@ -20,12 +20,7 @@ resource "proxmox_vm_qemu" "media-server" {
   name        = "media-server"
   desc        = "Debian Server for Media Server"
 
-  # VM Advanced General Settings
-  # onboot = true
-
-  # VM OS Settings
   clone = "debian11-ci-template-tailscale-ssh"
-  # full_clone = false
 
   # VM System Settings
   agent = 1
@@ -33,45 +28,16 @@ resource "proxmox_vm_qemu" "media-server" {
   # VM CPU Settings
   cores   = 2
   sockets = 2
-  # cpu     = "host"
 
   # VM Memory Settings
   memory  = 4098
   balloon = 1024
 
-  # VM Network Settings
-  # network {
-  #   bridge  = "vmbr0"
-  #   model   = "virtio"
-  #   macaddr = "52:A1:9D:2C:E1:C2"
-  # }
-  # disk {
-  #   size    = "70G"
-  #   type    = "scsi"
-  #   storage = "local-lvm"
-  # }
-
   # VM Cloud-Init Settings
   os_type = "cloud-init"
 
-  # (Optional) IP Address and Gateway
-  # ipconfig0 = "ip=192.168.1.102/32,gw=192.168.1.1"
-
-  # (Optional) Default User
   ciuser     = "ewan"
   cipassword = data.bitwarden_item_login.ewan_user_password.password
-  # cicustom   = "user=snippets:snippets/ubuntu-tailscale-docker.yml"
-  # cloudinit_cdrom_storage = "local-lvm"
-
-  # (Optional) Add your SSH KEY
-  # sshkeys = <<EOF
-  #   ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJMnq1UL0GsMFPHPr3D5GY81YUVM5c3YX64jhbW4Y6BS openpgp:0xDB92184E
-  #   EOF
-  # provisioner "remote-exec" {
-  #   inline = [
-  #     "ip a"
-  #   ]
-  # }
 }
 
 #TODO
